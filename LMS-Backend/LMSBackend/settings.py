cd27593871b8f772151f28fb90d7950d43b1e69c
Son Nguyen <hoangson091104@gmail.com>
2024-09-08 17:36:50 -0400

    Update: Enhanced Angular frontend

--- LMS-Backend/LMSBackend/settings.py
+++ LMS-Backend/LMSBackend/settings.py
@@ -20,13 +20,16 @@ connect(
     host="mongodb://localhost:27017/",
 )
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -175,10 +178,12 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
